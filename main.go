1049e72798aaba4ef56b66b94df65f60a3ba83fa
Henrique Palote <palote@rarolabs.com.br>
2020-05-15 23:18:33 -0300

    Added gin with post and get route

--- main.go
+++ main.go
@@ -1,7 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"github.com/gin-gonic/gin"
+)
 
-func main () {
-	fmt.Println("Hello world")
+type Todo struct {
+	Id          int    `json:"id"`
+	Name        string `json:"name" binding:"required"`
+	Description string `json:"description" binding: "required"`
+}
+
+var todoList []Todo
+
+func main() {
+	server := gin.Default()
+
+	server.GET("/todo", func(context *gin.Context) {
+		context.JSON(200, todoList)
+	})
+
+	server.POST("/todo", func(context *gin.Context) {
+		var todo Todo
+		context.BindJSON(&todo)
+		todo.Id = 1 // TODO: implement nextId
+		todoList = append(todoList, todo)
+
+		context.Status(201)
+	})
+
+	server.Run()
 }
